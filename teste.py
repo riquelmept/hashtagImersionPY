7d7e72c24d46a84021f4c43680a640e55d3b2aaa
Patrick Coelho <100944831+riquelmept@users.noreply.github.com>
2024-05-08 20:12:37 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,35 @@
+# Passo a passo do projeto
+# Passo 1: Entrar no sistema da empresa 
+    # https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
+import pyautogui
+import time
+
+# pyautogui.write -> escrever um texto
+# pyautogui.press -> apertar 1 tecla
+# pyautogui.click -> clicar em algum lugar da tela
+# pyautogui.hotkey -> combinação de teclas
+pyautogui.PAUSE = 0.5
+
+# abrir o navegador (chrome)
+pyautogui.press("win")
+pyautogui.write("brave")
+pyautogui.press("enter")
+
+# entrar no link 
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+time.sleep(3)
+
+
+# Passo 2: Fazer login
+# selecionar o campo de email
+pyautogui.click(x=-864, y=362)
+# escrever o seu email
+pyautogui.write("pythonimpressionador@gmail.com")
+pyautogui.press("tab") # passando pro próximo campo
+pyautogui.write("sua senha")
+
+pyautogui.press("tab")
+pyautogui.press("enter")
+time.sleep(3)
